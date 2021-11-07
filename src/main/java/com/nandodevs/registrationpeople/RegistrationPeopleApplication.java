4691c0d1e05ab87b3c00b07c6e0d5b0dbdb6280f
nandodevs <nando.devs@gmail.com>
2021-11-07 17:32:34 -0300

    Initial commit

---
+++ src/main/java/com/nandodevs/registrationpeople/RegistrationPeopleApplication.java
@@ -0,0 +1,13 @@
+package com.nandodevs.registrationpeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationPeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationPeopleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nandodevs/registrationpeople/RegistrationPeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nandodevs.registrationpeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationPeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
